6b0946e366967f9848258daca9f26f029626a8cd
object-ions <moshikoatia@gmail.com>
2023-10-07 20:44:25 -0700

    update Views and Controllers for Read and Create with EF Core

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -18,6 +18,25 @@ namespace HairSalon.Controllers
     {
       List<Stylist> stylists = _db.Stylists.ToList();
       return View(stylists);
-  }
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
   }
 }
